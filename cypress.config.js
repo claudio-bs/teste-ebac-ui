9fd8968d20bbdef0c30270dd89d8c9f808aa8938
Cláudio Santana <claudio_bs@icloud.com>
2023-09-13 16:52:44 -0300

    usando massa de dados em lista e relatórios

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: '9vv1q9',
   e2e: {
     baseUrl: "http://lojaebac.ebaconline.art.br/", setupNodeEvents(on, config) {
 
--- cypress/e2e/endereco.cy.js
+++ cypress/e2e/endereco.cy.js
@@ -1,5 +1,6 @@
 /// <reference types="cypress" />
 import EnderecoPage from '../support/page-objects/endereco.page'
+const dadosEndereco = require('../fixtures/endereco.json')
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     beforeEach(() => {
@@ -10,8 +11,23 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
 
     });
 
-    it.only('Deve fazer cadastro de faturamento com sucesso', () => {
+    it('Deve fazer cadastro de faturamento com sucesso', () => {
         EnderecoPage.editarEndrecoFaturamento('Kleber', 'Santana', 'Aplle', 'Brasil', 'Rua Vale do Silício', '8888', 'Araçatuba', 'São Paulo', '16057-560', '18123456789', 'email@ebacteste.com' )
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+    });
+
+    it('Deve fazer cadastro de faturamento com sucesso - Usando arquivo de dados', () => {
+        EnderecoPage.editarEndrecoFaturamento(dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].telefone,
+            dadosEndereco[1].email)
         cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
     });
 });
